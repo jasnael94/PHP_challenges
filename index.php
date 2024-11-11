9c3dad94d85b4551e480dfd476968c74f549b050
Jason <jasnael94@gmail.com>
2024-11-11 19:29:13 +0100

    adding challenge 6

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//challenge6
+
+function writeSecretSentence(string $parametre_1 , string $parametre_2):string
+{
+   return "$parametre_1 s'incline face à $parametre_2";
+  
+
+}
+
+echo writeSecretSentence("L'âne","l'arbre");
+
+?>
